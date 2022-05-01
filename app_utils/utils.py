880cb82bc39216f4fe2efcc5ad6c22ab1351d9e1
Diwanshu Asthana <diwanshu.asthana@gmail.com>
2022-05-01 22:59:25 +0530

    utils added

--- app_utils/utils.py
+++ app_utils/utils.py
@@ -1,3 +1,20 @@
 import logging
+import os, sys
+import yaml
+from app_logger.logger import log_function_signature
+from app_exception.exception import AppException
+
+@log_function_signature
+def read_yaml(yaml_file_path: str) -> dict:
+    try:
+        logging.info("Reading the config yaml file")
+        with open(yaml_file_path, 'rb') as yml:
+            config_dict = yaml.safe_load(yml)
+            logging.info("Configuration yaml file read successfully")
+            return config_dict
+    except Exception as e:
+        raise AppException(e, sys) from e
+
+
 
 
