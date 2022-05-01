667438d3d9b025231ca6308eb16875e1489babf7
Diwanshu Asthana <diwanshu.asthana@gmail.com>
2022-05-01 16:48:43 +0530

    updated

--- app_logger/logger.py
+++ app_logger/logger.py
@@ -1,24 +1,25 @@
 import logging
 import os
-from time import datetime
+from datetime import datetime
 import uuid
 
 EXPERIMENT_ID = str(uuid.uuid4())
-CURRENT_TIMESTAMP = datetime.now().stfrtime('%Y-%m-%d-%H-%M-%S')
+CURRENT_TIMESTAMP = datetime.now().strftime('%Y%m%d%H%M%S')
 FILENAME = f"log_{CURRENT_TIMESTAMP}.log"
 
 LOG_DIR = "logs"
 LOG_DIR = os.path.join(os.getcwd(), LOG_DIR)
 os.makedirs(LOG_DIR, exist_ok=True)
-LOG_FILE_PATH = os.path.join(LOG_DIR,FILENAME)
+LOG_FILE_PATH = os.path.join(LOG_DIR, FILENAME)
 
 logging.basicConfig(filename=LOG_FILE_PATH,
                     filemode="w",
-                    format='[%(asctime)s] - %(name) - %(level) - %(message)'
+                    format='[%(asctime)s] - %(name) - %(level) - %(message)',
                     level=logging.INFO)
 
+
 def log_function_signature(func):
-    def inner(*args,**kwargs):
+    def inner(*args, **kwargs):
         kw_args_text = ""
         for param_name, param_value in kwargs.items():
             kw_args_text = f"{kw_args_text},{param_name} = {param_value}"
@@ -28,5 +29,13 @@ def log_function_signature(func):
         response = func(*args, **kwargs)
         logging.info(f"Exiting {func.__name__}({arg_text}{kw_args_text})")
         return response
+
     return inner
 
+
+def print_text(text):
+    print(text)
+
+
+if __name__ == "__maim__":
+    print_text(Diwanshu)
