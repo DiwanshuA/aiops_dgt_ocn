e30da49f5692906fc0637d9521a5afac14f23f5e
Diwanshu Asthana <diwanshu.asthana@gmail.com>
2022-05-02 10:59:10 +0530

    configuration script created

--- app_configuration/configuration.py
+++ app_configuration/configuration.py
@@ -1,6 +1,21 @@
 import os, sys
-
 from collections import namedtuple
 from app_utils import read_yaml
 from app_exception import AppException
 from app_logger import log_function_signature
+from app_entity.config_entity import DatasetConfig, DataValidationConfig, PreprocessingConfig,\
+                                     ModelTrainingConfig, TrainingPipelineConfig
+
+ROOT_DIR = os.getcwd()
+CONFIG_FILENAME = "config.yaml"
+CONFIG_FILEPATH = os.path.join(ROOT_DIR, CONFIG_FILENAME)
+
+SCHEMA_FILENAME = "dataset_schema.yaml"
+SCHEMA_FILEPATH = os.path.join(ROOT_DIR, SCHEMA_FILENAME)
+
+DATASET_KEY = "data_set"
+DATASET_NAME_KEY = "name"
+SCHEMA_KEY = "schema"
+BUFFER_SIZE_KEY = "buffer_size"
+BATCH_SIZE_KEY = "batch_size"
+
